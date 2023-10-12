d9ec114764b8b6c3f781c274189d85daa996a8bd
mahmo <mahmo@DESKTOP-I8TTN18>
2023-10-12 13:09:27 -0400

    Add project files.

---
+++ MA/Item.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTax
+{
+    class Item
+    {
+        public string Name { get; }
+        public decimal Price { get; }
+        public bool IsImported { get; }
+        public bool IsExempt { get; }
+
+        public Item(string name, decimal price, bool isImported, bool isExempt)
+        {
+            Name = name;
+            Price = price;
+            IsImported = isImported;
+            IsExempt = isExempt;
+        }
+        public decimal GetSalesTax()
+        {
+            decimal salesTaxRate = 0m; 
+           if (!IsExempt)
+            {
+                salesTaxRate = 0.1m; // 10% basic sales tax
+            }
+            
+            if (IsImported)
+            {
+                salesTaxRate += 0.05m; // 5% import tax
+            }
+
+            decimal salesTax = Math.Ceiling(Price * salesTaxRate / 0.05m) * 0.05m; // Round off to nearest 5 cents
+            return salesTax;
+        }
+
+        public decimal GetTotalPrice()
+        {
+            decimal total = Price + GetSalesTax();
+            return total;
+        }
+    }
+    
+}
+
---
+++ MA/Program.cs
@@ -0,0 +1,48 @@
+﻿using SalesTax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Store!");
+        List<Item> shoppingBasket = new List<Item>();
+
+        while (true)
+        {
+            Console.Write("Enter item name (or 'done' to finish shopping): ");
+            string itemName = Console.ReadLine();
+            if (itemName.ToLower() == "done")
+                break;
+
+            Console.Write("Enter item price: ");
+            if (decimal.TryParse(Console.ReadLine(), out decimal itemPrice))
+            {
+                Console.Write("Is the item imported? (yes/no): ");
+                bool isImported = Console.ReadLine().ToLower() == "yes";
+
+                Console.Write("Is the item exempt from basic sales tax? (yes/no): ");
+                bool isExempt = Console.ReadLine().ToLower() == "yes";
+
+                shoppingBasket.Add(new Item(itemName, itemPrice, isImported, isExempt));
+            }
+            else
+            {
+                Console.WriteLine("Invalid price. Please enter a valid number.");
+            }
+        }
+
+        Console.WriteLine("Receipt:");
+        foreach (var item in shoppingBasket)
+        {
+            Console.WriteLine($"{item.Name}: {item.GetTotalPrice():F2}");
+        }
+
+        Console.WriteLine($"Sales Taxes: {shoppingBasket.Sum(item => item.GetSalesTax()):F2}");
+        Console.WriteLine($"Total: {shoppingBasket.Sum(item => item.GetTotalPrice()):F2}");
+    }
+    
+}
+
